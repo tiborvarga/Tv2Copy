72b4852346c17ca81b1945ac18d16201ca7f8c1c
Tibor Varga <tiborvarga@Tibis-MacBook-Pro.local>
2019-01-31 23:21:55 +0100

    01.31.

--- src/tv2copy/ViewFXMLController.java
+++ src/tv2copy/ViewFXMLController.java
@@ -1,5 +1,10 @@
 package tv2copy;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -9,11 +14,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
 
 public class ViewFXMLController implements Initializable {
     
     protected String clockNumber;
-    protected String urlName;
+    protected String username;
+    protected String password;
     protected String fileExt;
     
     @FXML
@@ -44,35 +52,89 @@ public class ViewFXMLController implements Initializable {
     @FXML
     private void submitButtonAction(ActionEvent event) {
         dataReader();
-        System.out.println(clockNumber + " " + urlName);       
+        fileGrabber();
+        System.out.println(clockNumber);       
     }
     
     private void dataReader(){
-        clockNumber = "";
-        urlName = "";
-        
-        String clockTemp = clockText.getText();
-        for (int i=0; i<clockTemp.length(); i++){
-            if (clockTemp.substring(i, i+1).equals("/") || clockTemp.substring(i, i+1).equals("\\"))
-                clockNumber = clockNumber + "_";
-            else
-                clockNumber = clockNumber + clockTemp.substring(i, i+1);
-        } 
-        
         RadioButton selectedRadioButton = (RadioButton) channelGroup.getSelectedToggle();
         String toogleGroupValue = selectedRadioButton.getId();
         
         switch (toogleGroupValue){
-            case "r2Radio": urlName = "2"; fileExt = "2"; break;
-            case "r2nRadio": urlName = "2n"; fileExt = "2n"; break;
-            case "r3Radio": urlName = "3"; fileExt = "3"; break;
-            case "r3nRadio": urlName = "3n"; fileExt = "3n"; break;
-            case "r4Radio": urlName = "4"; fileExt = "4"; break;
-            case "r4nRadio": urlName = "4n"; fileExt = "4n"; break;
-        }       
+            case "r2Radio": username = "adstream2spot"; password = "uQy63ogqNJ"; fileExt = ".mov"; break;
+            case "r2nRadio": username = "adstream2spot"; password = "uQy63ogqNJ"; fileExt = ".mov"; break;
+            case "r3Radio": username = "adstream3spot"; password = "Ab1Cd2Ef3"; fileExt = ".mpg"; break;
+            case "r3nRadio": username = "adstream3nonspot"; password = "Ab1Cd2Ef3"; fileExt = ".mpg"; break;
+            case "r4Radio": username = "adstream4spot"; password = "Wuser-5766"; fileExt = ".mov"; break;
+            case "r4nRadio": username = "adstream4nonspot"; password = "Kescy-2147"; fileExt = ".mov"; break;
+        } 
+        
+        clockNumber = "";
+        
+        String clockTemp = clockText.getText();
+        for (int i=0; i<clockTemp.length(); i++){
+            if (!clockTemp.substring(i, i+1).equals("/") && !clockTemp.substring(i, i+1).equals("\\") && !clockTemp.substring(i, i+1).equals("-") && !clockTemp.substring(i, i+1).equals("_"))
+                clockNumber = clockNumber + clockTemp.substring(i, i+1);
+            else{
+                switch (toogleGroupValue){
+                    case "r4Radio":                      
+                        if (clockTemp.substring(i, i+1).equals("/") || clockTemp.substring(i, i+1).equals("\\") || clockTemp.substring(i, i+1).equals("-") || clockTemp.substring(i, i+1).equals("_"))
+                            clockNumber = clockNumber + "-";
+                        break;
+                    case "r4nRadio":                      
+                        if (clockTemp.substring(i, i+1).equals("/") || clockTemp.substring(i, i+1).equals("\\") || clockTemp.substring(i, i+1).equals("-") || clockTemp.substring(i, i+1).equals("_"))
+                            clockNumber = clockNumber + "-";
+                        break; 
+                    default:
+                        if (clockTemp.substring(i, i+1).equals("/") || clockTemp.substring(i, i+1).equals("\\") || clockTemp.substring(i, i+1).equals("-") || clockTemp.substring(i, i+1).equals("_"))
+                            clockNumber = clockNumber + "_";
+                        break;
+                }
+            }
+        }     
     }
     
-    
+    public void fileGrabber(){
+        String server = "video.tv2.hu";
+        int port = 21;
+        String user = username;
+        String pass = password;
+ 
+        FTPClient ftpClient = new FTPClient();
+        
+        try {
+            ftpClient.connect(server, port);
+            ftpClient.login(user, pass);
+            ftpClient.enterLocalPassiveMode();
+            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+ 
+            // APPROACH #1: using retrieveFile(String, OutputStream)
+            String remoteFile = clockNumber + fileExt;
+            File downloadFile = new File("/Users/tiborvarga/Desktop/teszt/" + remoteFile);
+            OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(downloadFile));
+            boolean success = ftpClient.retrieveFile(remoteFile, outputStream);
+            outputStream.close();
+ 
+            if (success) {
+                System.out.println("File #1 has been downloaded successfully.");
+            }
+        }
+        catch (IOException ex) {
+            System.out.println("Error: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+        finally {
+            try {
+                if (ftpClient.isConnected()) {
+                    ftpClient.logout();
+                    ftpClient.disconnect();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
